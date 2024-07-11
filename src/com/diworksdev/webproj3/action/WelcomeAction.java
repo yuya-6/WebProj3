b7b00947c00506ded5f8f44eb692bf6c7259ea5d
yuya-6 <yuchongakufu@gmail.com>
2024-07-11 15:05:25 +0900

    HelloStruts機能（成功）

---
+++ src/com/diworksdev/webproj3/action/WelcomeAction.java
@@ -0,0 +1,9 @@
+package com.diworksdev.webproj3.action;
+import com.opensymphony.xwork2.ActionSupport;
+
+
+public class WelcomeAction extends ActionSupport {
+	public String execute() {
+		return SUCCESS;
+		}
+}
